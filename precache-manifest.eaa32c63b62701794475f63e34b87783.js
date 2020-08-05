86b7a244a9e35e477e52c1ec92388447bce9dee3
max <salimova.mekhri@gmail.com>
2020-08-06 00:26:59 +0500

    Updates

---
+++ precache-manifest.eaa32c63b62701794475f63e34b87783.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abd7f39b025f7a4c34721a53ed70a4a",
+    "url": "/ReactPizza/index.html"
+  },
+  {
+    "revision": "ac9cee64fc7aa716289b",
+    "url": "/ReactPizza/static/css/main.0f24f101.chunk.css"
+  },
+  {
+    "revision": "8567a8c18e97d1a258a2",
+    "url": "/ReactPizza/static/js/2.b968cb57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPizza/static/js/2.b968cb57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac9cee64fc7aa716289b",
+    "url": "/ReactPizza/static/js/main.04f4a1d4.chunk.js"
+  },
+  {
+    "revision": "544db1ae120fe9a09136",
+    "url": "/ReactPizza/static/js/runtime-main.0722ef0b.js"
+  },
+  {
+    "revision": "c6fdc55725637e34b900b8f2810b2e3a",
+    "url": "/ReactPizza/static/media/black_cart.c6fdc557.svg"
+  },
+  {
+    "revision": "ae0815b87e2f0d664ded9e61e8f7dd27",
+    "url": "/ReactPizza/static/media/cancel.ae0815b8.svg"
+  },
+  {
+    "revision": "118838167f52e918fc044b97cc42e081",
+    "url": "/ReactPizza/static/media/cart.11883816.svg"
+  },
+  {
+    "revision": "cb31982108d245211611a77f5afcb924",
+    "url": "/ReactPizza/static/media/empty.cb319821.svg"
+  },
+  {
+    "revision": "3bf5a3d74e3e4cdb104553a18da827b6",
+    "url": "/ReactPizza/static/media/face.3bf5a3d7.svg"
+  },
+  {
+    "revision": "5d7ef77b3ccc0b384d7b33cb4ba94dcd",
+    "url": "/ReactPizza/static/media/garbage.5d7ef77b.svg"
+  },
+  {
+    "revision": "91542c760608cb158d21ce477a84a8a5",
+    "url": "/ReactPizza/static/media/logo.91542c76.svg"
+  },
+  {
+    "revision": "65ea14b370e473663b575552a195f9f7",
+    "url": "/ReactPizza/static/media/pizza1.65ea14b3.jpg"
+  },
+  {
+    "revision": "9ec65e7ca22843933d8a7c72588f18f3",
+    "url": "/ReactPizza/static/media/pizza2.9ec65e7c.jpg"
+  },
+  {
+    "revision": "62b07402d39fa1c743f3f551822b3815",
+    "url": "/ReactPizza/static/media/pizza3.62b07402.jpg"
+  },
+  {
+    "revision": "605e4f83363e01e4fe2e33cfb4b99024",
+    "url": "/ReactPizza/static/media/pizza4.605e4f83.jpg"
+  }
+]);
