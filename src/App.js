a184c4295cd2b444bec8c1df114821d5c42ca5f3
max <salimova.mekhri@gmail.com>
2020-08-06 00:43:57 +0500

    modified

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import store from './redux/redux-store';
 
 const App = (props) => {
   return (
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Provider store = {store}>
         <div className="App">
           <div className='wrapper'>
